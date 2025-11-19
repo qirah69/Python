62ede3994a51e83d78daaabfeed95f53ff7702d2
qirah69 <merluscastefan10@gmail.com>
2025-11-19 14:10:28 +0200

    aw

---
+++ Book_Management/domain.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, id, title, description, author):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.author = author
